87546e478859a55b61e1dc2499f0da380269522e
David Ventura <dventura@chorus.one>
2025-02-02 12:27:24 +0100

    move more flags to toplevel cmake

--- app/bergamot/build.gradle.kts
+++ app/bergamot/build.gradle.kts
@@ -18,9 +18,7 @@ android {
             }
         }
         ndk {
-            abiFilters += listOf("x86_64", "arm64-v8a") // armeabi-v7a arm64-v8a
-//            abiFilters += listOf("arm64-v8a") // armeabi-v7a arm64-v8a
-
+            abiFilters += listOf("x86_64", "arm64-v8a")
         }
     }
 

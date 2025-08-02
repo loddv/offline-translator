0e602384317b33854fe46d1f81d0b984d488a3b5
David Ventura <david@davidv.dev>
2025-08-02 17:27:12 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 28 // iconv functions need 28?
         targetSdk = 34
         versionCode = 1
-        versionName = "0.0.6"
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

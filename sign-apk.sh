829b640f171048095a5e2b9d80fb1da6185eca8c
David Ventura <david@davidv.dev>
2025-08-03 12:19:23 +0200

    update sign script to use apksigner

--- sign-apk.sh
+++ sign-apk.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <keystore_path> <store_password> <key_password> <key_alias> <version_name>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <keystore_path> <store_password> <key_password> <key_alias>"
     exit 1
 fi
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ]; then
-    echo "Usage: $0 <keystore_path> <store_password> <key_password> <key_alias> <version_name>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+    echo "Usage: $0 <keystore_path> <store_password> <key_password> <key_alias>"
     exit 1
 fi
 
@@ -17,7 +17,6 @@ KEYSTORE_PATH="$1"
 STORE_PASSWORD="$2"
 KEY_PASSWORD="$3"
 KEY_ALIAS="$4"
-VERSION_NAME="$5"
 
 # Find the unsigned APK
 unsigned_apk=$(find app/build/outputs/apk -name "*-release-unsigned.apk" | head -1)
@@ -26,22 +25,24 @@ if [ -z "$unsigned_apk" ]; then
     exit 1
 fi
 
+VERSION_NAME=$(aapt dump badging "$unsigned_apk"  | grep -oP "versionName='\K(.*?)'" | tr -d "'")
 echo "Signing APK: $unsigned_apk"
 
+# Align the APK first (required before signing with apksigner)
+aligned_apk="${unsigned_apk%.apk}-aligned.apk"
+rm -f "$aligned_apk"
+$ANDROID_SDK_ROOT/build-tools/34.0.0/zipalign 4 "$unsigned_apk" "$aligned_apk"
+
 # Sign the APK
-jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 \
-    -keystore "$KEYSTORE_PATH" \
-    -storepass "$STORE_PASSWORD" \
-    -keypass "$KEY_PASSWORD" \
-    "$unsigned_apk" \
-    "$KEY_ALIAS"
-
-# Align the APK
 mkdir -p signed
-rm -f "signed/app-release-signed-$VERSION_NAME.apk"
-$ANDROID_SDK_ROOT/build-tools/34.0.0/zipalign -v 4 "$unsigned_apk" "signed/app-release-signed-$VERSION_NAME.apk"
+signed_fname="signed/translator-$VERSION_NAME.apk"
+rm -f "$signed_fname"
+$ANDROID_SDK_ROOT/build-tools/34.0.0/apksigner sign \
+    --ks "$KEYSTORE_PATH" \
+    --ks-pass pass:"$STORE_PASSWORD" \
+    --ks-key-alias "$KEY_ALIAS" \
+    --key-pass pass:"$KEY_PASSWORD" \
+    --out "$signed_fname" \
+    "$aligned_apk"
 
-# Verify the signature
-jarsigner -verify -verbose -certs "signed/app-release-signed-$VERSION_NAME.apk"
-
-echo "APK successfully signed: signed/app-release-signed-$VERSION_NAME.apk"
+echo "APK successfully signed: $signed_fname"

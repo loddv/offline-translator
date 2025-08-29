ad03cf5f56da0706da3beb9376c1432b0a256ab0
David Ventura <david@davidv.dev>
2025-08-29 11:02:32 +0200

    bump to v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     applicationId = "dev.davidv.translator"
     minSdk = 28 // iconv functions need 28?
     targetSdk = 34
-    versionCode = 2
-    versionName = "0.1.0"
+    versionCode = 3
+    versionName = "0.1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
